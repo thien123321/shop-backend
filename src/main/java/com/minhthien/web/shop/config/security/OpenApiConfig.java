c3455bdd410fc7f0700360f1df84c03dd023601f
MinhThien31 <thienndmse181738@fpt.edu.vn>
2026-03-02 09:04:30 +0300

    Config  hmm

--- src/main/java/com/minhthien/web/shop/config/security/OpenApiConfig.java
+++ src/main/java/com/minhthien/web/shop/config/security/OpenApiConfig.java
@@ -1,15 +1,11 @@
 package com.minhthien.web.shop.config.security;
 
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -17,9 +13,6 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .servers(List.of(
-                        new Server().url("https://shop-backend-production-3924.up.railway.app")
-                ))
 
                 .components(
                         new Components()
